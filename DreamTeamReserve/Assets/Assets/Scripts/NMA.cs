79016b42991180f8b9b2165a236be7db06dd7177
F1Nnelox <timurganzha7@gmail.com>
2019-11-30 16:08:44 +0300

    Almost Fixed NavMesh

---
+++ DreamTeamReserve/Assets/Assets/Scripts/NMA.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NMA : MonoBehaviour
+{
+    public NavMeshAgent vrag;
+    public GameObject Player;
+
+
+    void Start()
+    {
+        vrag = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if (Player != null)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            if (distance < 2.0f)
+            {
+                Debug.Log("Поц тебя ударил");
+            }
+            else if (distance < 6f)
+            {
+                vrag.destination = player.transform.position;
+            }
+        }
+
+    }
+}
