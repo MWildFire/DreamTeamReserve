ac850a4cc8ca5cdae825176b80395622d3cae8b0
Pocik <nikitaagafonov228@mail.ru>
2019-11-30 16:38:15 +0300

    MainMenu Added something new

---
+++ DreamTeamReserve/Assets/Assets/Scripts/MainMenu_Menu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_Menu : MonoBehaviour
+{
+    public GameObject GamePanel;
+    public GameObject OptionsPanel;
+    public GameObject AudioPanel;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    public void Game()
+    {
+        GamePanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+        AudioPanel.SetActive(false);
+    }
+    public void Options()
+    {
+        GamePanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+        AudioPanel.SetActive(false);
+    }
+    public void Audio()
+    {
+        GamePanel.SetActive(false);
+        OptionsPanel.SetActive(false);
+        AudioPanel.SetActive(true);
+    }
+}
