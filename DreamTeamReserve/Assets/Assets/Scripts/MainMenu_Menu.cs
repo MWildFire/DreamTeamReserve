bc9439e28219bdc659212301f856cf7c9d8a782e
Pocik <nikitaagafonov228@mail.ru>
2019-12-01 17:54:06 +0300

    SomeFixes

--- DreamTeamReserve/Assets/Assets/Scripts/MainMenu_Menu.cs
+++ DreamTeamReserve/Assets/Assets/Scripts/MainMenu_Menu.cs
@@ -2,38 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenu_Menu : MonoBehaviour
+namespace lol
 {
-    public GameObject GamePanel;
-    public GameObject OptionsPanel;
-    public GameObject AudioPanel;
-    void Start()
+    public class MainMenu_Menu : MonoBehaviour
     {
-        
-    }
+        public AllOptionsPreferences PermanentOptionsPreferences;
+        public GameObject ObjectOptionsPreferences;
 
-    
-    void Update()
-    {
+        public GameObject MainMenuPanel;
+        public GameObject OthersPanel;
+        public GameObject OptionsPanel;
+        public GameObject GamePanel;
+        public GameObject GraphicPanel;
+        public GameObject AudioPanel;
+        public GameObject Black;
 
-    }
+        void Start()
+        {
 
-    public void Game()
-    {
-        GamePanel.SetActive(true);
-        OptionsPanel.SetActive(false);
-        AudioPanel.SetActive(false);
-    }
-    public void Options()
-    {
-        GamePanel.SetActive(false);
-        OptionsPanel.SetActive(true);
-        AudioPanel.SetActive(false);
-    }
-    public void Audio()
-    {
-        GamePanel.SetActive(false);
-        OptionsPanel.SetActive(false);
-        AudioPanel.SetActive(true);
+        }
+
+
+        void Update()
+        {
+
+        }
+
+        public void StartGame()
+        {
+            
+        }
+
+        public void BackButton()
+        {
+            OptionsPanel.SetActive(false);
+            Black.SetActive(false);
+            MainMenuPanel.SetActive(true);
+            OthersPanel.SetActive(false);
+        }
+
+        public void OptionsButton()
+        {
+            MainMenuPanel.SetActive(false);
+            OptionsPanel.SetActive(true);
+            OthersPanel.SetActive(true);
+            Black.SetActive(true);
+        }
+
+        public void Game()
+        {
+            GamePanel.SetActive(true);
+            GraphicPanel.SetActive(false);
+            AudioPanel.SetActive(false);
+            Black.SetActive(false);
+        }
+        public void Graphic()
+        {
+            GamePanel.SetActive(false);
+            GraphicPanel.SetActive(true);
+            AudioPanel.SetActive(false);
+            Black.SetActive(false);
+        }
+        public void Audio()
+        {
+            GamePanel.SetActive(false);
+            GraphicPanel.SetActive(false);
+            AudioPanel.SetActive(true);
+            Black.SetActive(false);
+        }
     }
 }
