be300238978479375dc6dd0efa74ff8b42fd3dc6
Pocik <nikitaagafonov228@mail.ru>
2019-12-01 19:44:34 +0300

    Some fixes some added

---
+++ DreamTeamReserve/Assets/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+}
--- DreamTeamReserve/Assets/Assets/Scripts/MainMenu_Menu.cs
+++ DreamTeamReserve/Assets/Assets/Scripts/MainMenu_Menu.cs
@@ -31,6 +31,7 @@ namespace lol
         public void StartGame()
         {
             
+            
         }
 
         public void BackButton()
