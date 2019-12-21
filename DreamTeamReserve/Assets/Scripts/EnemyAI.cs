4bdb3a4612bc2074cb8b1ae26d93847c158ac3c9
MVStrike <mvstrike17@gmail.com>
2019-12-21 17:15:25 +0300

    Lol

---
+++ DreamTeamReserve/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform[] pos;
+
+
+    private NavMeshAgent agent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        int value = Random.Range(0, 3);
+        if (other.gameObject.CompareTag("Point"))
+        {
+            agent.destination = pos[value].position;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
